5745590d2995edb47c322a78addd04a8f86eeea9
Rachelami <rachelami@gmail.com>
2020-10-07 15:34:43 +0300

    split weather info to a new component

--- src/components/home/CitiesContainer.js
+++ src/components/home/CitiesContainer.js
@@ -3,11 +3,9 @@ import { connect } from 'react-redux'
 import { fetchCities } from '../../redux/getCity/getCityActions'
 import { fetchcurrentWeather } from '../../redux/getCurrentWeather/getCurrentWeatherActions'
 import { useSelector, useDispatch } from 'react-redux'
+import Weatherinfo from './WeatherInfo'
 
 function CitiesContainer({ weatherData, citiesData, fetchCities }) {
-    const [expended, setExpended] = useState(false)
-    const [presentFahrenheit, setPresentFahrenheit] = useState(false)
-
     const dispatch = useDispatch() //added
 
     useEffect(() => {
@@ -23,14 +21,10 @@ function CitiesContainer({ weatherData, citiesData, fetchCities }) {
         console.log(locationKey)
 
         dispatch(fetchcurrentWeather(locationKey)) //added
-        setExpended(true)
 
     }
 
-    const capitalize = (string) => {
-        if (typeof string !== 'string') return ''
-        return string.charAt(0).toUpperCase() + string.slice(1)
-      }
+
 
     return (
         citiesData.loading ? <h2>Loading...</h2> :
@@ -44,24 +38,9 @@ function CitiesContainer({ weatherData, citiesData, fetchCities }) {
                                 <>
                                     <div className="location-card">
                                         <button key={city.key} onClick={() => callFetchWeather(city.Key)} >{city.LocalizedName}</button>
-                                        {weatherData &&
-                                        weatherData.currentWeather[0]&&
-                                            <div className="weather-strip-container">
-                                                <div className="weather-strip">
-                                                    <div className="weather-info-container">
-                                                        {/* < div>{capitalize(cityWeather.cityName)}</ div> */}
-                                                        <div className="flex">
-                                                            <img src={process.env.PUBLIC_URL + `/images/weather-icons/${weatherData.currentWeather[0].WeatherIcon}.svg`} className="temp-logos" />
-                                                            <div>{capitalize(weatherData.currentWeather[0].WeatherText)}</div>
-                                                        </div>
-                                                        {presentFahrenheit ?
-                                                            <div>{Math.round(weatherData.currentWeather[0].Temperature.Imperial.Value)}&deg;F</div> :
-                                                            <div>{Math.round(weatherData.currentWeather[0].Temperature.Metric.Value)}&deg;C</div>
-                                                        }
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        }
+                                        
+                                            <Weatherinfo />
+                                      
                                     </div>
                                 </>
                             )}</div>
--- src/components/home/WeatherInfo.js
+++ src/components/home/WeatherInfo.js
@@ -1,13 +1,49 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { connect } from 'react-redux'
 
-const Weatherinfo = () => {
-    
+const Weatherinfo = ({ weatherData }) => {
+    const [presentFahrenheit, setPresentFahrenheit] = useState(false)
+
+    const capitalize = (string) => {
+        if (typeof string !== 'string') return ''
+        return string.charAt(0).toUpperCase() + string.slice(1)
+    }
+
+    useEffect(() => {
+        console.log(weatherData)
+    })
 
     return (
         <>
-            
+            {weatherData &&
+                weatherData.currentWeather[0] &&
+                <div className="weather-strip-container">
+                    <div className="weather-strip">
+                        <div className="weather-info-container">
+                            {/* < div>{capitalize(cityWeather.cityName)}</ div> */}
+                            <div className="flex">
+                                <img src={process.env.PUBLIC_URL + `/images/weather-icons/${weatherData.currentWeather[0].WeatherIcon}.svg`} className="temp-logos" />
+                                <div>{capitalize(weatherData.currentWeather[0].WeatherText)}</div>
+                            </div>
+                            {presentFahrenheit ?
+                                <div>{Math.round(weatherData.currentWeather[0].Temperature.Imperial.Value)}&deg;F</div> :
+                                <div>{Math.round(weatherData.currentWeather[0].Temperature.Metric.Value)}&deg;C</div>
+                            }
+                        </div>
+                    </div>
+                </div>
+            }
         </>
     )
 }
 
-export default Weatherinfo
+const mapStateToProps = state => {
+    return {
+        citiesData: state.cities,
+        weatherData: state.weather
+    }
+}
+
+export default connect(
+    mapStateToProps
+)(Weatherinfo)
